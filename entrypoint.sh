5c39de29644f5eab68634890e494506788ec7924
Slava Borysyuk <borysyy@sunypoly.edu>
2023-12-18 17:33:00 -0500

    Fixed

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <source_code_file.cpp>"
+    exit 1
+fi
+
+g++ -o user_program "$1" 
+
+if [ $? -eq 0 ]; then
+    ./user_program > /output/output.txt
+else
+    echo "Compilation failed."
+fi
