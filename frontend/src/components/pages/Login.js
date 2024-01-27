2eb2b6f65c47883782c746db122391e5d461c7d5
Michael Giardina <mgiardina02@gmail.com>
2024-01-27 10:32:41 -0500

    init

---
+++ frontend/src/components/pages/Login.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Login = () => {
+  return (
+    <div className="container text-center mt-4">
+      <h1>Hello world </h1>
+    </div>
+  );
+};
+
+export default Login;
